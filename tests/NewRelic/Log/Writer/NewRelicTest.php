039aa528a1de68edfa922cecd8dac84a1258ba71
James Webber <bunkerprivate@gmail.com>
2014-01-16 18:49:49 +0000

    Log writer looks for an exception object in event.extra.

--- tests/NewRelic/Log/Writer/NewRelicTest.php
+++ tests/NewRelic/Log/Writer/NewRelicTest.php
@@ -3,16 +3,37 @@ namespace NewRelic\Log\Writer;
 
 class NewRelicTest extends \PHPUnit_Framework_TestCase
 {
-    public function testSetClient()
+    public function testMessageLogged()
     {
+        $loggedMessage = "foo";
         $client = $this->getMock('NewRelic\Client', array(), array(), '', false);
         $client
             ->expects($this->once())
-            ->method('noticeError');
+            ->method('noticeError')
+            ->with($this->equalTo($loggedMessage));
 
         $writer = new NewRelic($client);
         $writer->write(array(
-            'message' => 'foo',
+            'message' => $loggedMessage,
+        ));
+    }
+
+    public function testExceptionAndMessageLogged()
+    {
+        $loggedMessage = "foo";
+        $loggedError = new \Exception();
+        $client = $this->getMock('NewRelic\Client', array(), array(), '', false);
+        $client
+            ->expects($this->once())
+            ->method('noticeError')
+            ->with($this->equalTo($loggedMessage), $this->equalTo($loggedError));
+
+        $writer = new NewRelic($client);
+        $writer->write(array(
+            'message' => $loggedMessage,
+            'extra' => array(
+                'exception' => $loggedError,
+            ),
         ));
     }
 }
