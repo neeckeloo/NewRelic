4b8782735d4c2851470bff290b931f44a1a996d9
Mark Garrett <mark@moderndeveloperllc.com>
2015-01-15 10:22:41 -0600

    Actually make the fix
    Actually remove the directory. GitHub messed me up.

--- src/Module.php
+++ src/Module.php
@@ -13,7 +13,7 @@ class Module implements
 {
     public function getConfig()
     {
-        return include __DIR__ . '/../../config/module.config.php';
+        return include __DIR__ . '/../config/module.config.php';
     }
 
     public function getAutoloaderConfig()
