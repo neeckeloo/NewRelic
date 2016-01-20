d23e08d9c5d658423edb4821b58385ee2906eb06
Nicolas Eeckeloo <neeckeloo@gmail.com>
2016-01-20 14:18:06 +0100

    Use PSR-3 compatible logger

--- tests/Factory/ErrorListenerFactoryTest.php
+++ tests/Factory/ErrorListenerFactoryTest.php
@@ -3,27 +3,22 @@ namespace NewRelicTest\Factory;
 
 use NewRelic\Factory\ErrorListenerFactory;
 use NewRelic\Listener\ErrorListener;
-use Zend\Log\Logger;
+use Psr\Log\LoggerInterface;;
 use Zend\ServiceManager\ServiceManager;
 
 class ErrorListenerFactoryTest extends \PHPUnit_Framework_TestCase
 {
-    /**
-     * @var ErrorListenerFactory
-     */
-    protected $errorListenerFactory;
-
-    public function setUp()
-    {
-        $this->errorListenerFactory = new ErrorListenerFactory();
-    }
-
     public function testCreateService()
     {
         $serviceManager = new ServiceManager();
-        $serviceManager->setService('NewRelic\Logger', new Logger());
+        $serviceManager->setService(
+            'NewRelic\Logger',
+            $this->getMock(LoggerInterface::class)
+        );
+        $errorListenerFactory = new ErrorListenerFactory();
 
-        $listener = $this->errorListenerFactory->createService($serviceManager);
+        $listener = $errorListenerFactory->createService($serviceManager);
+
         $this->assertInstanceOf(ErrorListener::class, $listener);
     }
 }
